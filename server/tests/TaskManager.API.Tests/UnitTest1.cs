b6e404d1ae831832822ecb6a26b4543f15d8b8db
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2025-06-20 13:57:48 -0300

    Scaffold the basic project and create docker files

---
+++ server/tests/TaskManager.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ server/tests/TaskManager.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
