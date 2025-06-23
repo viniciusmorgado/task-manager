2d269f9c76741a134ba2e4d995c726b0dee1e913
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2025-06-23 16:33:49 -0300

    Solve merge conflict

--- server/src/TaskManager.Infrastructure/obj/Debug/net9.0/TaskManager.Infrastructure.AssemblyInfo.cs
+++ server/src/TaskManager.Infrastructure/obj/Debug/net9.0/TaskManager.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea92b85149267985103cfeda7de23540e45846fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9144d505ed38fdcd944efa76f5903f3e0175ab5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
